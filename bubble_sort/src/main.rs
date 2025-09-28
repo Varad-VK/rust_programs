59ca5afd96ffcbdb37f6be8b79f378fbca5e7a39
Varad Kulkarni <varadk.work@gmail.com>
2025-09-28 22:52:56 +0530

    bubble sort

---
+++ bubble_sort/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut arr = [50,20,40,30,10,80,60,90,70];
+    let n= arr.len();
+
+    for i in 0..n{
+        for j in 0..(n-i-1){
+            if arr[j]>arr[j+1]{
+                arr.swap(j, j+1);
+            }
+        }
+        
+    }
+    println!("{arr:?}");
+}
