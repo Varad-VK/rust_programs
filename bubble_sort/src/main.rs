e74d10786b3b3258b29c8a694b3226933d1fe5ea
Varad Kulkarni <varadk.work@gmail.com>
2025-10-04 15:55:09 +0530

    new algorithm : Bubble sort

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -3,10 +3,15 @@ fn main() {
     let n= arr.len();
 
     for i in 0..n{
+        let mut is_swap = false; // to achive O(n) time complexity if array is already sorted
         for j in 0..(n-i-1){
             if arr[j]>arr[j+1]{
                 arr.swap(j, j+1);
+                is_swap = true;
             }
+        }
+        if !is_swap{
+            break;
         }
         
     }
